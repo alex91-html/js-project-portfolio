755e742ec16992009580f197095fafe90fd325c7
alex91-html <alex91-html@users.noreply.github.com>
2025-04-23 18:23:34 +0200

    updated App.jsx with SkillsSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ProjectsSection from "./components/section-project/ProjectsSection.jsx";
+import SkillsSection from "./components/section-skills/SkillsSection.jsx";
+
 
 export const App = () => {
   return (
     <div>
+      <SkillsSection />
       <ProjectsSection />
     </div>
   );
