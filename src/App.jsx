4df7739f98e1fa8e940a7e3d304c561875c4efc4
alex91-html <alex91-html@users.noreply.github.com>
2025-04-24 16:19:55 +0200

    adding info section at bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import ProjectsSection from "./components/section-project/ProjectsSection.jsx";
 import SkillsSection from "./components/section-skills/SkillsSection.jsx";
 import TechSection from "./components/section-tech/TechSection.jsx";
+import AboutSection from "./components/section-about/AboutSection.jsx";
+import HeroSection from "./components/section-hero/HeroSection.jsx";
 
 
 
 export const App = () => {
   return (
     <div>
+      <HeroSection />
       <TechSection />
-      <SkillsSection />
       <ProjectsSection />
+      <AboutSection />
+      <SkillsSection />
     </div>
   );
 };
---
+++ src/components/section-hero/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1>Alex is a designer who uses code and currently freelances as web developer and Graphic Designer based in Stockholm. You can contact him via email or instagram.
+      </h1>
+    </section>
+  )
+}
+export default HeroSection;
