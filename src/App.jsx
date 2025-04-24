34ae424571002f87f523dfe817405d07928b3fc8
alex91-html <alex91-html@users.noreply.github.com>
2025-04-24 14:46:51 +0200

    TechSection.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ProjectsSection from "./components/section-project/ProjectsSection.jsx";
 import SkillsSection from "./components/section-skills/SkillsSection.jsx";
+import TechSection from "./components/section-tech/TechSection.jsx";
+
 
 
 export const App = () => {
   return (
     <div>
+      <TechSection />
       <SkillsSection />
       <ProjectsSection />
     </div>
--- src/components/section-tech/TechSection.jsx
+++ src/components/section-tech/TechSection.jsx
@@ -1,2 +1,18 @@
 import React from 'react';
 import techData from '../../data/tech.json';
+
+const TechSection = () => {
+  return (
+    <section>
+      <h2>Tech</h2>
+      <ul>{techData.tech.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+      </ul>
+
+    </section>
+  );
+
+};
+
+export default TechSection;
