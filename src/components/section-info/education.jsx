28a6420f5efbe17c8e36ff9d2c8abe89df0188d7
alex91-html <alex91-html@users.noreply.github.com>
2025-05-02 15:46:05 +0200

    finished styling Education

--- src/components/section-info/education.jsx
+++ src/components/section-info/education.jsx
@@ -3,16 +3,34 @@ import styled from "styled-components";
 import infoData from "../../data/info.json";
 import SectionTitle from "../common/SectionTitle";
 
+const EducationWrapper = styled.div``;
+const EducationList = styled.ul``;
+const EducationItem = styled.li`
+  font-size: ${({ theme }) => theme.fontSizes.body};
+  color: ${({ theme }) => theme.colors.text};
+  line-height: 1.6;
+
+  & > span.school {
+    font-style: italic;
+  }
+`;
+
 const Education = () => {
+  const education = infoData.education?.content || [];
   return (
-    <div>
-      <h2>{infoData.education.title}</h2>
-      <ul>
-        {infoData.education.content.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <EducationWrapper>
+      <SectionTitle title={infoData.education?.title || "Education"} />
+      <EducationList>
+        {education.map((item, index) => {
+          const [school, program] = item.split(" – ");
+          return (
+            <EducationItem key={index}>
+              <span className="school">{school}</span> – {program}
+            </EducationItem>
+          );
+        })}
+      </EducationList>
+    </EducationWrapper>
   );
 };
 
